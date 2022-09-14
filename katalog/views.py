2dd4bb35f9e8a19cdfdffc3ea5e634267a97fb42
stenly10 <yosuasaputra.stenly@gmail.com>
2022-09-14 21:08:34 +0700

    mengerjakan tugas 2

--- katalog/views.py
+++ katalog/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from katalog.models import CatalogItem
 
 # TODO: Create your views here.
+def show_katalog(request):
+    data_barang_katalog = CatalogItem.objects.all()
+    context = {
+        'nama': 'Stenly Yosua Saputra',
+        'id': '2106704244',
+        'list_barang': data_barang_katalog
+    }
+    return render(request, "katalog.html", context)
