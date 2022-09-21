c38de0d17ea153afb1379dd0c837e7a1e49c0f59
stenly10 <yosuasaputra.stenly@gmail.com>
2022-09-22 03:19:02 +0700

    Update Tugas 3

--- mywatchlist/tests.py
+++ mywatchlist/tests.py
@@ -1,3 +1,15 @@
 from django.test import TestCase
+from django.test import Client
 
-# Create your tests here.
+class TestingTugas(TestCase):
+    def testing_html(self):
+        html_response = Client().get("/mywatchlist/html/")
+        self.assertEqual(html_response.status_code, 200)
+
+    def testing_xml(self):
+        xml_response = Client().get("/mywatchlist/xml/")
+        self.assertEqual(xml_response.status_code, 200)
+
+    def testing_json(self):
+        json_response = Client().get("/mywatchlist/json/")
+        self.assertEqual(json_response.status_code, 200)
